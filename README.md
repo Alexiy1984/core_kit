01d61cf3e34e13d4f43a6defbb32503d2f22ea57
Alexiy1984 <alevaliev@yandex.ru>
2020-10-28 19:17:59 +0300

    initial commit:css and html generation TODO: add js generation and structure improvement

---
+++ README.md
@@ -0,0 +1,3 @@
+# core_kit
+
+## Core UI kit for themes progect
---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+const { src, dest, watch, parallel, series } = require('gulp');
+
+const pug = require('gulp-pug'),
+data = require('gulp-data'),
+sync = require('browser-sync').create(),
+fs = require('fs'),
+sass = require('gulp-sass'),
+concat = require('gulp-concat'),
+autoprefixer = require('gulp-autoprefixer'),
+plumber = require('gulp-plumber');
+
+var sassOptions = {
+  outputStyle: 'expanded',
+};
+
+var prefixerOptions = {
+  browserlist: ['last 3 versions']
+};
+ 
+function  buildHTML(cb) {
+  src('views/*.pug')
+    .pipe(plumber())
+    .pipe(data(function(file) {
+      return JSON.parse(fs.readFileSync('./data/data.json'))
+    }))
+    .pipe(pug({
+      doctype: 'html',
+      pretty: true
+    }))
+    .pipe(dest('public/'))
+    .pipe(sync.stream());
+  cb();
+}
+
+function generateCSS(cb) {
+  src('./scss/*.scss')
+      .pipe(sass(sassOptions).on('error', sass.logError))
+      .pipe(autoprefixer(prefixerOptions))
+      // .pipe(concat('index.css'))
+      .pipe(dest('public/stylesheets'))
+      .pipe(sync.stream());
+  cb();
+}
+
+function browserSync(cb) {
+  sync.init({
+      server: {
+          baseDir: './public'
+      }
+  });
+
+  watch('./views/*.pug', buildHTML);
+  watch('./views/**/*.pug', buildHTML);
+  watch('./scss', generateCSS);
+  watch('./public/**.html').on('change', sync.reload);
+}
+
+function defaultTask(cb) {
+  // place code for your default task here
+  cb();
+}
+
+exports.default = defaultTask;
+exports.pug = buildHTML;
+exports.sync = browserSync;
+exports.css = generateCSS;
