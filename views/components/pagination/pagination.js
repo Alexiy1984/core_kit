6a248474e56460d8f4e0d4aa619c0eecb1378e9b
Alex <alevaliev@yandex.ru>
2020-11-10 17:22:55 +0300

    added pagination script

--- views/components/pagination/pagination.js
+++ views/components/pagination/pagination.js
@@ -1,8 +1,48 @@
 $(function() {
-  $('.pagination .page-item').on('click', function () {
-    $(this).siblings
-      .removeClass('active')
-    $(this)
-      .addClass('active')
+  $('.pagination .page-item').not('.control, .disabled, .delimiter').on('click', function () {
+    if ($(this).next().hasClass('control')) {
+      $(this).prevAll(':visible:not(.control):not(.delimiter)').hide();
+      $(this).prevAll().slice(1, 4).show();
+      $(this).siblings('.page-item:not(.control)')
+        .removeClass('active')
+      $(this).not('.control')
+        .addClass('active')
+    } else {
+      $(this).siblings('.page-item:not(.control)')
+        .removeClass('active')
+      $(this).not('.control')
+        .addClass('active')
+    }
+  });
+
+  $('.pagination .page-item.control').on('click', function () {
+    var active_elt = $(this).parent().find('.page-item.active');
+    if ($(this).attr('data-direction') === 'next' && !active_elt.next().hasClass('control')) {
+      if (active_elt.next().is(':hidden')) {
+        active_elt.prevAll('.page-item:not(.control)').slice(1).hide();
+        active_elt.removeClass('active');
+        active_elt.next().show().addClass('active');
+      } else {
+        if (active_elt.next().hasClass('delimiter')) {
+          active_elt.removeClass('active').next().next().addClass('active');
+        } else {
+          active_elt.removeClass('active').next().addClass('active');
+        }
+      }
+    }
+
+    if ($(this).attr('data-direction') === 'prev' && !active_elt.prev().hasClass('control')) {
+      if (active_elt.prev().is(':hidden')) {
+        active_elt.nextAll(':visible').slice(1, 2).hide();
+        active_elt.removeClass('active');
+        active_elt.prev().show().addClass('active');
+      } else {
+        if (active_elt.prev().hasClass('delimiter')) {
+          active_elt.removeClass('active').prev().prev().addClass('active');
+        } else {
+          active_elt.removeClass('active').prev().addClass('active');
+        }
+      }
+    }    
   });
 });
