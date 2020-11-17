fcbcb78497547769db821b19995a3ee3ed0a7310
Alex <alevaliev@yandex.ru>
2020-11-17 17:52:02 +0300

    re #6 added rounded button and shell script for componrnt create

---
+++ mk_component.sh
@@ -0,0 +1,3 @@
+mkdir -p views/components/$1 
+cp views/components/_blank/_extended.pug views/components/$1/index.pug
+touch views/components/$1/$1.pug
--- views/index.js
+++ views/index.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('.btn-group > * , .btn-group_primary > * ,.btn-group_succes > * ,.btn-group_warning > * ,.btn-group_danger > * ,.btn-group_colorfull > * ')
+  $('.btn-group > * , .btn-group_primary > * ,.btn-group_success > * ,.btn-group_warning > * ,.btn-group_danger > * ,.btn-group_colorfull > * ')
   .on('click', function () {
     if (!$(this).hasClass('button_disabled') && !$(this).hasClass('disabled')) {
       $(this).toggleClass('selected');
